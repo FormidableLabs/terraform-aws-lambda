b81a8b41ceebc5567e887d0f77d71ac080c5398b
Paul Marsicovetere <paul.marsicovetere@formidable.com>
2022-12-01 11:15:52 -0500

    adding in lambda testing

--- test/terraform_aws_lambda_test.go
+++ test/terraform_aws_lambda_test.go
@@ -1,9 +1,11 @@
 package test
 
 import (
+	"os"
+	"os/exec"
 	"testing"
 
-	"github.com/gruntwork-io/terratest/modules/aws"
+	// "github.com/gruntwork-io/terratest/modules/aws"
 	"github.com/gruntwork-io/terratest/modules/terraform"
 	"github.com/stretchr/testify/assert"
 )
@@ -11,6 +13,7 @@ import (
 func TestTerraformAWSLambda(t *testing.T) {
 	// Construct the terraform options with default retryable errors to handle the most common
 	// retryable errors in terraform testing.
+
 	terraformOptions := terraform.WithDefaultRetryableErrors(t, &terraform.Options{
 		// Set the path to the Terraform code that will be tested.
 		TerraformDir: "../",
@@ -25,17 +28,22 @@ func TestTerraformAWSLambda(t *testing.T) {
 	// Run `terraform output` to get the values of output variables and check they have the expected values.
 	lambdaArn := terraform.Output(t, terraformOptions, "arn")
 	assert.Equal(t, "arn:aws:lambda:us-east-1:000000000000:function:use1-development-testLambda", lambdaArn)
-	functionName := terraform.Output(t, terraformOptions, "function_name")
-	awsRegion := "us-east-1"
-
-	testEvent := &HelloWorld{
-		Key: "value1",
+	// Obtain working directory
+	dirPath, err := os.Getwd()
+	if err != nil {
 	}
 
-	testResponse := aws.InvokeFunction(t, awsRegion, functionName, testEvent)
-	assert.NotEmpty(t, testResponse)
-}
+	// Test invoking Lambda function
+	invokeLambdaCmd := "awslocal lambda invoke --function-name use1-development-testLambda response.json"
+	exec.Command("bash", "-c", invokeLambdaCmd).Output()
 
-type HelloWorld struct {
-	Key string `json:"Key"`
+	// if response.json is returned, that means the Lambda Invocation was successful
+	assert.FileExists(t, dirPath+"/response.json")
+
+	// Confirm CloudWatch Logs group created successfully
+	cloudwatchLogsCmd := "awslocal logs describe-log-groups --log-group-name-prefix /aws/lambda/use1-development-testLambda | jq -r '.logGroups[].logGroupName'"
+	out, err := exec.Command("bash", "-c", cloudwatchLogsCmd).Output()
+
+	assert.Equal(t, "/aws/lambda/use1-development-testLambda\n", string(out))
+
 }
